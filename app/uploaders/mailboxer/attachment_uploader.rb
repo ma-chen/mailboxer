0fd33f425977fc4d2812c0c403b583fd7384a65a
Ma Chen <ma-chen@outlook.com>
2017-01-05 16:51:49 +0800

    attachment uploader - small version of images

--- app/uploaders/mailboxer/attachment_uploader.rb
+++ app/uploaders/mailboxer/attachment_uploader.rb
@@ -1,5 +1,11 @@
 class Mailboxer::AttachmentUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   def store_dir
     "uploads/messages/#{model.conversation_id}/#{model.id}"
+  end
+
+  version :small do
+    process resize_to_fill: [200,200]
   end
 end
