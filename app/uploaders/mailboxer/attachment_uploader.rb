4b926f852ff5b03eb3c427d09b564e1a4daebf03
Ma Chen <ma-chen@outlook.com>
2017-01-18 22:55:07 +0800

    image attachment - medium version

--- app/uploaders/mailboxer/attachment_uploader.rb
+++ app/uploaders/mailboxer/attachment_uploader.rb
@@ -9,13 +9,27 @@ class Mailboxer::AttachmentUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [200,200]
   end
 
-  version :medium, :if => :image? do
+  version :medium, :if => :image? && :is_landscape? do
     process resize_to_fit: [415,-1]
+  end
+
+  version :medium, :if => :image? && :is_portrait? do
+    process resize_to_fit: [-1,215]
   end
 
   protected
 
   def image?(new_file)
     new_file.content_type.start_with? 'image'
+  end
+
+  def is_landscape? picture
+    image = MiniMagick::Image.open(picture.path)
+    image[:width] >= image[:height]
+  end
+
+  def is_portrait? picture
+    image = MiniMagick::Image.open(picture.path)
+    image[:width] < image[:height]
   end
 end
