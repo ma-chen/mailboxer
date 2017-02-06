7467cd72f5eecad17705b9af1a72240dc04e85b1
Ma Chen <ma-chen@outlook.com>
2017-02-06 10:23:08 +0800

    search kick

--- app/models/mailboxer/receipt.rb
+++ app/models/mailboxer/receipt.rb
@@ -32,7 +32,7 @@ class Mailboxer::Receipt < ActiveRecord::Base
 
   obfuscate_id
 
-  searchkick settings: {index: {max_result_window:1000000}}
+  searchkick settings: {index: {max_result_window:1000000}}, text_middle: [:subject, :body]
   def search_data
     attributes.merge(
       conversation_id: notification.conversation_id,
